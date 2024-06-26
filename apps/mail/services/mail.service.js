7c909b09b7a086713c292b79bd213c6c8a18435f
Denis Libin <dnslibin4@gmail.com>
2024-06-26 12:16:40 +0300

    Add mail service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,127 @@
 // mail service
+
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const MAIL_KEY = 'mailDB'
+_createMails()
+
+export const mailService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyMail,
+    getDefaultFilter,
+    getFilterFromSearchParams
+}
+
+function query(filterBy = {}) {
+    return storageService.query(MAIL_KEY)
+        .then(mails => {
+            console.log('mails:', mails)
+
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                mails = mails.filter(mail => regExp.test(mail.vendor))
+            }
+            if (filterBy.minSpeed) {
+                mails = mails.filter(mail => mail.speed >= filterBy.minSpeed)
+            }
+            return mails
+        })
+}
+
+function get(mailId) {
+    return storageService.get(MAIL_KEY, mailId)
+        .then(mail => _setNextPrevMailId(mail))
+}
+
+function remove(mailId) {
+    return storageService.remove(MAIL_KEY, mailId)
+}
+
+function save(mail) {
+    if (mail.id) {
+        return storageService.put(MAIL_KEY, MAIL)
+    } else {
+        return storageService.post(MAIL_KEY, mail)
+    }
+}
+
+// function getEmptyMail(vendor = '', speed = '') {
+//     return { vendor, speed }
+// }
+
+function getDefaultFilter() {
+    return { txt: '', minSpeed: '' }
+}
+
+
+function getFilterFromSearchParams(searchParams) {
+    // return Object.fromEntries(searchParams)
+    const txt = searchParams.get('txt') || ''
+    const minSpeed = searchParams.get('minSpeed') || ''
+    return {
+        txt,
+        minSpeed
+    }
+}
+
+function _createMails() {
+    let mails = utilService.loadFromStorage(MAIL_KEY)
+    if (!mails || !mails.length) {
+        mails = []
+        mails.push(_createMail())
+        mails.push(_createMail())
+        mails.push(_createMail())
+        mails.push(_createMail())
+        utilService.saveToStorage(MAIL_KEY, mails)
+    }
+}
+
+function _createMail() {
+    const mail = {
+        id: utilService.makeId(),
+        createdAt: 1551133930500,
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false,
+        sentAt: 1551133930594,
+        removedAt: null,
+        from: 'momo@momo.com',
+        to: 'user@appsus.com'
+    }
+        mail.id = utilService.makeId()
+    return mail
+}
+
+
+function _setNextPrevMailId(mail) {
+    return storageService.query(MAIL_KEY).then((mails) => {
+        const mailIdx = mails.findIndex((currMail) => currMail.id === mail.id)
+        const nextMail = mails[mailIdx + 1] ? mails[mailIdx + 1] : mails[0]
+        const prevMail = mails[mailIdx - 1] ? mails[mailIdx - 1] : mails[mails.length - 1]
+        mail.nextMailId = nextMail.id
+        mail.prevMailId = prevMail.id
+        return mail
+    })
+}
+
+const loggedInUser = {
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus'
+}
+
+const email = {
+    id: 'e101',
+    createdAt: 1551133930500,
+    subject: 'Miss you!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt: 1551133930594,
+    removedAt: null,
+    from: 'momo@momo.com',
+    to: 'user@appsus.com'
+}
+
