a3673204eec6fd13e5bdafd17caa64e9e199d737
Denis Libin <dnslibin4@gmail.com>
2024-06-26 14:11:12 +0300

    Add MailIndex, MailList, MailPreview. Working demo data

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,34 @@
+import { mailService } from "../apps/mail/services/mail.service.js"
+
+const { useEffect, useState } = React
 
 export function MailIndex() {
-    return <div>mail app</div>
-}
 
+    const [mails, setMails] = useState(null)
+
+    useEffect(() => {
+        loadMails()
+    }, [])
+    
+    function loadMails() {
+        carService.query()
+            .then(mails => {
+                setMails(mails)
+            })
+            .catch(err => {
+                console.log('err:', err)
+            })
+    }
+
+    if (!cars) return <div>Loading...</div>
+    return (
+        <section className="mail-index">
+            {/* <button><Link to="/car/edit">Add Car</Link></button> */}
+            {/* <CarFilter filterBy={filterBy} onSetFilter={onSetFilter} /> */}
+            <MailList
+                cars={cars}
+                // onRemoveCar={onRemoveCar}
+            />
+        </section>
+    )
+}
