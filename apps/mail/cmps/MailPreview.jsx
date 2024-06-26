f6b2fa5ef5cf0ac93e61257e67599d10502c1ca6
Denis Libin <dnslibin4@gmail.com>
2024-06-26 14:41:04 +0300

    Check

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -6,6 +6,7 @@ export function MailPreview({ mail }) {
         <article className="car-preview">
             <span>Subject: {subject}</span>
             <h4>Body: {body}</h4>
+            
             <h5>From: {from}</h5>
             {/* <img src={`../assets/img/${car.vendor}.png`} alt="" /> */}
         </article>
