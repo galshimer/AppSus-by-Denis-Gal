53cf4542395f7bbf80954043bf6e36ebb38b764a
Denis Libin <dnslibin4@gmail.com>
2024-06-27 10:07:19 +0300

    Add styling to mails

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,17 +1,22 @@
 const { Link } = ReactRouterDOM
 
 export function MailPreview({ mail, onRemoveMail }) {
-
     const { subject, body, from } = mail
     return (
         <article className="mail-preview">
-            <span>{from} {subject} {body}
-                <button onClick={() => onRemoveMail(mail.id)}>Remove</button>
-                <button><Link to={`/mail/${mail.id}`}>Details</Link></button>
-            </span>
+            <div className="mail-info">
+                <span className="mail-from">{from}</span>
+                <span className="mail-subject">{subject}</span>
+                <span className="mail-body">{body}</span>
+            </div>
+            <div className="mail-actions">
+                <button className="remove-btn" onClick={() => onRemoveMail(mail.id)}>Remove</button>
+                <button className="details-btn"><Link to={`/mail/${mail.id}`}>Details</Link></button>
+            </div>
         </article>
     )
 }
+
 // id: utilService.makeId(),
 //         createdAt: 1551133930500,
 //         subject: 'Miss you!',
