d83e4ffd89b22bad072cf33f7fd91fbfef095be2
Gal Shimer <galshimer2@gmail.com>
2024-06-29 12:56:00 +0300

    Home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,13 @@
+const { Link} = ReactRouterDOM
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+        <h1>Welcome to  our home page!</h1>
+        <img className="logo" src="./assets/img/Logo.png"/>
+        <h2> Which app would you like to choose?</h2>
+        <div className="navigation-links">
+                <Link to="/mail" className="nav-link"><img className='logo-gmail home-logo' src="./assets/img/logo-gmail.png"/></Link>
+                <Link to="/note" className="nav-link"><img className='logo-keep home-logo' src="./assets/img/logo-keep.png"/></Link>
+            </div>
     </section>
 }
